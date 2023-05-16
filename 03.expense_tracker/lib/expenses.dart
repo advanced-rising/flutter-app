0703c3c8576277866900638e715bb6d6ffef19b3
risingcore <vcjsm2283@gmail.com>
2023-05-16 23:21:37 +0900

    to make listview widget

--- 03.expense_tracker/lib/expenses.dart
+++ 03.expense_tracker/lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,11 +11,24 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Cinema',
+        amount: 16.59,
+        date: DateTime.now(),
+        category: Category.leisure)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [const Text('The chart'), Text('The list')],
+        children: const [Text('The chart'), Text('The list')],
       ),
     );
   }
