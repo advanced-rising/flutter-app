92c088b2c760486053087bc52beb05251f9869ea
risingcore <vcjsm2283@gmail.com>
2023-05-18 22:49:55 +0900

    add modal sheet bottom handler

--- 03.expense_tracker/lib/widgets/expenses.dart
+++ 03.expense_tracker/lib/widgets/expenses.dart
@@ -1,13 +1,14 @@
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
-import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
-  State<StatefulWidget> createState() {
+  State<Expenses> createState() {
     return _ExpensesState();
   }
 }
@@ -15,24 +16,44 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(
-        title: 'Course',
-        amount: 19.99,
-        date: DateTime.now(),
-        category: Category.food),
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
     Expense(
-        title: 'Cinema',
-        amount: 16.59,
-        date: DateTime.now(),
-        category: Category.leisure)
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
--- 03.expense_tracker/lib/widgets/expenses_list/expense_item.dart
+++ 03.expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
+import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
-import 'package:flutter/material.dart';
 
 class ExpenseItem extends StatelessWidget {
   const ExpenseItem(this.expense, {super.key});
@@ -12,29 +11,27 @@ class ExpenseItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
         child: Column(
           children: [
             Text(expense.title),
-            const SizedBox(
-              height: 4,
-            ),
+            const SizedBox(height: 4),
             Row(
               children: [
                 Text('\$${expense.amount.toStringAsFixed(2)}'),
-                // 12.4343 => 12.43
                 const Spacer(),
                 Row(
                   children: [
-                    Icon(Icons.alarm),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(expense.date.toString())
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
                   ],
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
